78ce2593268466dcfe2715c63668ae05b93d72d5
Blaine F Garrett <blaine@blainegarrett.com>
2017-07-31 21:47:17 -0500

    Prepping for release.

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const PREF_SERVICE_DOMAIN = 'http://localhost:9090';
-//export const PREF_SERVICE_DOMAIN = 'https://pref-service-dev.appspot.com';
+//export const PREF_SERVICE_DOMAIN = 'http://localhost:9090';
+export const PREF_SERVICE_DOMAIN = 'https://pref-service-dev.appspot.com';
